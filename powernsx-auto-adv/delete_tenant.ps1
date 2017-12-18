e0c08bba53d1add98a31fc77895a186006d7fcee
gowatana <gowatana@vmtn.jp>
2017-12-19 01:14:31 +0900

    テナント削除スクリプト追加

---
+++ powernsx-auto-adv/delete_tenant.ps1
@@ -0,0 +1,31 @@
+﻿$tenant_nw_config =  $args[0]
+
+# テナントの ID を指定
+$tenant_no = $args[1] #1～254
+
+. $tenant_nw_config
+
+$ls = Get-NsxTransportZone $tz_name | Get-NsxLogicalSwitch -Name $ls_name
+
+"Remove VM"
+$ls | Get-NsxBackingPortGroup | Get-VM | Stop-VM -Confirm:$false | Out-Null
+$ls | Get-NsxBackingPortGroup | Get-VM | Remove-VM -DeletePermanently -Confirm:$false
+
+"Remove DFW Rule"
+Get-NsxFirewallSection -Name $dfw_section_name |
+    Remove-NsxFirewallSection -force -Confirm:$false
+
+"Remove SNAT Rule"
+$nat_original_addr = $nw_addr + "/" + $nw_msak_length
+Get-NsxEdge -objectId $esg_id | Get-NsxEdgeNat | Get-NsxEdgeNatRule |
+    where {$_.originalAddress -eq $nat_original_addr} |
+    Remove-NsxEdgeNatRule -Confirm:$false
+
+"Remove NsxLogical Switch"
+Get-NsxLogicalRouter -objectId $dlr_id | Get-NsxLogicalRouterInterface |
+    where {$_.connectedToId -eq $ls.objectId} |
+    Remove-NsxLogicalRouterInterface -Confirm:$false
+$ls | Remove-NsxLogicalSwitch -Confirm:$false
+
+"Remove VM Folder"
+Get-Folder -Type VM $tenant_name | Remove-Folder -Confirm:$false
